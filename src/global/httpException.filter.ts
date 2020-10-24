3f7388713270b0bee767184aede0be1d32e72703
dangphu2412 <dangphu241299@gmail.com>
2020-10-24 17:03:22 +0700

    fix httpexception

--- src/global/httpException.filter.ts
+++ src/global/httpException.filter.ts
@@ -1,7 +1,5 @@
 import {ExceptionFilter, Catch, ArgumentsHost, HttpException} from "@nestjs/common";
 import {Response} from "express";
-import {TErrorResponse, TErrorBody} from "src/common/type/t.Error";
-import {toError} from "src/utils";
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
@@ -9,11 +7,9 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
-    const errorResponse = exception.getResponse() as TErrorResponse;
-    console.log(errorResponse)
-    const error: TErrorBody = toError(errorResponse);
+    const errorResponse = exception.getResponse();
     response
       .status(status)
-      .json(error);
+      .json(errorResponse);
   }
 }
